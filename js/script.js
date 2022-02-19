54c84e79c5efd72848ce394a234370754d9da600
Krishandeep <krishandeep17@gmail.com>
2022-02-20 02:14:50 +0530

    Fixed Flexbox Gap Property

--- js/script.js
+++ js/script.js
@@ -116,22 +116,3 @@ form.addEventListener("submit", function (e) {
     form.submit();
   }
 });
-
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
-// function checkFlexGap() {
-//   var flex = document.createElement("div");
-//   flex.style.display = "flex";
-//   flex.style.flexDirection = "column";
-//   flex.style.rowGap = "1px";
-
-//   flex.appendChild(document.createElement("div"));
-//   flex.appendChild(document.createElement("div"));
-
-//   document.body.appendChild(flex);
-//   var isSupported = flex.scrollHeight === 1;
-//   flex.parentNode.removeChild(flex);
-
-//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
-// }
-// checkFlexGap();
